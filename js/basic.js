54e957880d3479c5273a71ca74720efcd008d148
chroniclegames <80473716+chroniclegames@users.noreply.github.com>
2023-08-28 00:59:26 -0500

    Update basic.js

--- js/basic.js
+++ js/basic.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     let testEntityAdded = false;
-
+    alert('test load');
     const el = document.querySelector("[gps-new-camera]");
 
     el.addEventListener("gps-camera-update-position", e => {
